aec004d47aa5d8ed420f78e9c007793539a33ef9
Jonathan B <31107158+jballo@users.noreply.github.com>
2026-04-23 13:21:13 -0700

    Todo: Added error handling + initial db creation on fastapi app startup

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,9 @@
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlmodel import Field, Session, SQLModel, create_engine
+from sqlalchemy import exc
+from contextlib import asynccontextmanager
 
 class Settings(BaseSettings):
     database_url: str
@@ -11,8 +13,8 @@ class Settings(BaseSettings):
 class User(SQLModel, table=True):
     __tablename__ = "users"
 
-    id: str = Field(default=None, primary_key=True)
-    email: str = Field(default=None)
+    id: str = Field(primary_key=True)
+    email: str
 
 settings = Settings()
 engine = create_engine(settings.database_url)
@@ -21,14 +23,25 @@ def get_session():
     with Session(engine) as session:
         yield session
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
 SessionDep = Annotated[Session, Depends(get_session)]
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/users")
 def create_user(user: User, session: SessionDep) -> User:
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-    return user
+    try:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return user
+    except exc.IntegrityError:
+        session.rollback()
+        raise HTTPException(status_code=409, detail="Already exists")
+        
+
