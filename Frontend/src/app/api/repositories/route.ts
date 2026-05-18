6d247fc4ca875bede5a88bc6600ae71c9fe9715e
Jonathan B <31107158+jballo@users.noreply.github.com>
2026-05-18 16:45:48 -0700

    Todo: Removed unused POST handler for 'Frontend/src/app/api/repositories/route.ts'

--- Frontend/src/app/api/repositories/route.ts
+++ Frontend/src/app/api/repositories/route.ts
@@ -1,21 +1,6 @@
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  try {
-    const body = await req.json();
-    const { url } = body;
-
-    if (!url || url.length === 0) throw new Error(`Failure processing`);
-
-    console.log("Repo url: ", url);
-    return new NextResponse(`Success processing ${url}`, { status: 200 });
-  } catch (error) {
-    console.error("[/api/repositories]: ", error);
-    return new NextResponse(`System failed`, { status: 500 });
-  }
-}
-
 export async function GET() {
   try {
     const { isAuthenticated, userId } = await auth();
